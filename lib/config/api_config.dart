8b6a02bc1c740782ccc265a79b5a551422795c8e
ceskdsgn <ceskdsgn@gmail.com>
2025-06-18 00:45:12 +0200

    Update api_config.dart

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,8 +1,7 @@
 class ApiConfig {
   // IMPORTANTE: Sostituire con la propria chiave API OpenAI
   // Per sicurezza, in produzione usare variabili d'ambiente
-  static const String openAIApiKey =
-      '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';
+  static const String openAIApiKey = '';
 
   // URL base per le API OpenAI
   static const String openAIBaseUrl =
