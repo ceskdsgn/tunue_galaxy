40c63b34b4c69652f8c845c5a46aa02d3052c219
ceskdsgn <ceskdsgn@gmail.com>
2025-06-19 21:47:46 +0200

    Update theme.dart

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -35,7 +35,7 @@ class AppTheme {
         padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
       ),
     ),
-    cardTheme: CardTheme(
+    cardTheme: CardThemeData(
       elevation: 2,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(12),
@@ -119,7 +119,7 @@ class AppTheme {
         padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
       ),
     ),
-    cardTheme: CardTheme(
+    cardTheme: CardThemeData(
       elevation: 4,
       color: const Color(0xFF1E1E1E),
       shape: RoundedRectangleBorder(
